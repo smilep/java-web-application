aca3dfd268807f0518d1b86e5c35685ba4e389d4
Smile <smile.pabby@gmail.com>
2018-09-30 17:01:18 +0530

    Initial commit for springboot app

---
+++ src/main/java/com/smilep/java/webapp/ServiceImplApplication.java
@@ -0,0 +1,12 @@
+package com.smilep.java.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceImplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceImplApplication.class, args);
+	}
+}
