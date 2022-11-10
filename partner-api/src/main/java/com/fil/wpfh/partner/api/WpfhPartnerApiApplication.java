9cdaa31211a399062a54619d3ea3d1ee28ac1be6
smilep <smile.pabby@gmail.com>
2022-11-10 12:49:21 +0530

    Added partner API

---
+++ partner-api/src/main/java/com/fil/wpfh/partner/api/WpfhPartnerApiApplication.java
@@ -0,0 +1,13 @@
+package com.fil.wpfh.partner.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WpfhPartnerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WpfhPartnerApiApplication.class, args);
+	}
+
+}
---
+++ partner-api/src/test/java/com/fil/wpfh/partner/api/WpfhPartnerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fil.wpfh.partner.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WpfhPartnerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
